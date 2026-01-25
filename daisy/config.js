ac086d5c223d4774bb97588f04d35a8fa4f6c30e
Lovejot <140100951+lovejot5@users.noreply.github.com>
2026-01-26 01:02:39 +0530

    Update config.js

--- daisy/config.js
+++ daisy/config.js
@@ -9,10 +9,10 @@ const DAISY_CONFIG = {
     docs: "/daisy/docs",
 
     invite:
-      "https://discord.com/oauth2/authorize?client_id=YOUR_CLIENT_ID&permissions=8&scope=bot%20applications.commands",
+      "https://discord.com/oauth2/authorize?client_id=1128750014613704754&permissions=8836764482928609&response_type=code&redirect_uri=https%3A%2F%2Fdiscord.gg%2FRqQsbNE7G4&integration_type=0&scope=bot+guilds",
 
     support:
-      "https://discord.gg/YOUR_SUPPORT_SERVER",
+      "https://discord.gg/RqQsbNE7G4",
   },
   footer: {
     text: "Daisy © 2026 · All rights reserved",
